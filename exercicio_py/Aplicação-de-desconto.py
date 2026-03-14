10a7084dda5fb2a6098208d7231d7e96000ef826
kauan moraes <kauanmoraesouza@gmail.com>
2026-03-14 19:54:07 -0300

    verificar_desconto.py

---
+++ exercicio_py/Aplicação-de-desconto.py
@@ -0,0 +1,5 @@
+preço = int(input("Digite o preço do produto: "))
+if preço > 100:
+    print("Desconto aplicado.")
+else:
+    print("Compra sem desconto.")
--- exercicio_py/avalidor-de-idade.py
+++ exercicio_py/avalidor-de-idade.py
@@ -1,6 +1,7 @@
 
 idade = int(input("Digite a idade: "))
+
 if idade < 18:
-    print("Conta não permitida para menores de idade.")
+     print("Conta não permitida para menores de idade.")
 else:
-    print("Conta autorizada.")
+     print("Conta autorizada.")
