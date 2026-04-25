387b6b56e996c1ff4c4a3852704f870d664de622
kauan moraes <kauanmoraesouza@gmail.com>
2026-04-25 17:08:32 -0300

    operacoes_conjuntos.py

---
+++ exercicio3_py/Operações-com-sets.py
@@ -0,0 +1,13 @@
+A = {1, 2, 3}
+B = {3, 4, 5}
+C = A & B  # Interseção
+D = A | B  # União
+E = A - B  # Diferença
+F = B - A  # Diferença
+
+print("A:", A)
+print("B:", B)
+print("Interseção (A & B):", C)
+print("União (A | B):", D)
+print("Diferença (A - B):", E)
+print("Diferença (B - A):", F)
