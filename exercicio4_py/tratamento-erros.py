c939d4f80a7ab46c0dd3ab772e5c4def10fac1ae
kauan moraes <kauanmoraesouza@gmail.com>
2026-05-07 18:24:16 -0300

    tratamento_erros.py

---
+++ exercicio4_py/tratamento-erros.py
@@ -0,0 +1,9 @@
+try:
+    numero = int(input('digite qualquer numero : '))
+    print(f'seu {numero}')
+except (ValueError):
+        print('Digite um número válido.')
+        
+        
+        
+        
